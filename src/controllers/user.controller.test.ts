b759218fc1f70232497c15499b83c6ff138bca71
Anton Halauko <anton.halauko.main@gmail.com>
2023-03-10 13:13:15 +0200

    Implement tests for user controller getUserById method

--- src/controllers/user.controller.test.ts
+++ src/controllers/user.controller.test.ts
@@ -5,20 +5,18 @@ import type { IValidatorProvider } from '@validators/models/validators-provider.
 import type { PassportAuthenticator } from '@authenticator/passport.authenticator';
 import type { UserServiceProvider } from '@components/user/user-service.provider';
 import type { UserService } from '@components/user/user.service';
-import type { IUserDatabaseModel } from '@components/user/user.models';
+import { IUserDatabaseModel, UserServiceResult, User } from '@components/user/user.models';
 import { UserController } from '@controllers/user.controller';
 import { ErrorHandlerData } from '@common/models/error-handler-data.models';
 
 describe('User Controller', () => {
     let databaseProvider: IDatabaseProvider;
     let validatorProvider: IValidatorProvider;
-
     let authenticator: PassportAuthenticator;
-    let userServiceProvider: UserServiceProvider;
 
     let userService: UserService;
-
     let userController: UserController;
+    let userServiceProvider: UserServiceProvider;
 
     let request: Request;
     let response: Response;
@@ -53,13 +51,15 @@ describe('User Controller', () => {
         response = {
             send: jest.fn(() => response),
             status: jest.fn(() => response),
+            json: jest.fn(() => response),
+            locals: {},
         } as unknown as Response;
         next = jest.fn();
 
         userController = new UserController(databaseProvider, validatorProvider, authenticator, userServiceProvider);
     });
 
-    describe(('Login method'), () => {
+    describe('Login method', () => {
         const TEST_USER_CREDENTIALS: Partial<IUserDatabaseModel> = {
             login: 'login1@mail.com',
             password: 'Password123',
@@ -78,7 +78,6 @@ describe('User Controller', () => {
             const statusMock = (response.status as jest.Mock).mock;
             const statusFirstCall = statusMock.calls[0];
             const firstCallArgument = statusFirstCall[0];
-
             expect(firstCallArgument).toBe(StatusCodes.OK);
         });
 
@@ -93,7 +92,6 @@ describe('User Controller', () => {
             const sendMock = (response.send as jest.Mock).mock;
             const sendMockFirstCall = sendMock.calls[0];
             const firstCallArgument = sendMockFirstCall[0];
-
             expect(firstCallArgument).toBe(token);
         });
 
@@ -105,13 +103,95 @@ describe('User Controller', () => {
             await userController.login(request, response, next);
 
             const nextMock = (next as jest.Mock).mock;
-
             expect(nextMock.calls).toHaveLength(1);
 
             const nextMockFirstCall = nextMock.calls[0];
             const firstCallArgument = nextMockFirstCall[0];
-
             expect(firstCallArgument).toBeInstanceOf(ErrorHandlerData);
         })
+    });
+
+    describe('getUserById method', () => {
+        const TEST_USER: User = new User({
+            id: 'user_id',
+            login: 'login1@mail.com',
+            password: 'Password123',
+            age: 15,
+        });
+
+        beforeEach(() => {
+            userService.getUserById = async (id: string) => {
+                if (id === TEST_USER.id) {
+                    return new UserServiceResult<User>({ data: TEST_USER });
+                }
+                return new UserServiceResult<User>({ error: 'User does not exist' });
+            };
+        });
+
+        test(`the status should be ${StatusCodes.OK}`, async () => {
+            request.params = { id: TEST_USER.id };
+
+            await userController.getUserById(request, response, next);
+
+            const statusMock = (response.status as jest.Mock).mock;
+            const statusFirstCall = statusMock.calls[0];
+            const firstCallArgument = statusFirstCall[0];
+            expect(firstCallArgument).toBe(StatusCodes.OK);
+        });
+
+        test('should send found user', async () => {
+            request.params = { id: TEST_USER.id };
+
+            await userController.getUserById(request, response, next);
+
+            const jsonMock = (response.json as jest.Mock).mock;
+            const jsonFirstCall = jsonMock.calls[0];
+            const firstCallArgument = jsonFirstCall[0];
+            expect(firstCallArgument).toBe(TEST_USER);
+        });
+
+        test('should call next function without arguments', async () => {
+            request.params = { id: TEST_USER.id };
+
+            await userController.getUserById(request, response, next);
+
+            const nextMock = (next as jest.Mock).mock;
+            expect(nextMock.calls).toHaveLength(1);
+
+            const nextMockFirstCall = nextMock.calls[0];
+            const firstCallArgument = nextMockFirstCall[0];
+            expect(firstCallArgument).toBe(undefined);
+        });
+
+        test(
+            'in case of error should call next function with one',
+            async () => {
+                request.params = { id: 'some' };
+
+                await userController.getUserById(request, response, next);
+
+                const nextMock = (next as jest.Mock).mock;
+                expect(nextMock.calls).toHaveLength(1);
+
+                const nextMockFirstCall = nextMock.calls[0];
+                const firstCallArgument = nextMockFirstCall[0];
+                expect(firstCallArgument).toBeInstanceOf(ErrorHandlerData);
+            },
+        );
+
+        test(
+            'in case of error should not call status and json response methods',
+            async () => {
+                request.params = { id: 'some' };
+
+                await userController.getUserById(request, response, next);
+
+                const statusMock = (response.status as jest.Mock).mock;
+                expect(statusMock.calls).toHaveLength(0);
+
+                const jsonMock = (response.json as jest.Mock).mock;
+                expect(jsonMock.calls).toHaveLength(0);
+            },
+        );
     });
 });
